04fbda442e613816df4944801bc92e77001b42ff
karl_eludo <k_eludo@icloud.com>
2022-02-09 20:51:26 +0800

    generate the landing page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ButtonComponent } from './components/button/button.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ButtonComponent
+    ButtonComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
