f306ec55d7259ff12da99a2358a7714c6c613a27
karl_eludo <k_eludo@icloud.com>
2022-02-16 21:29:55 +0800

    added routing component with routing to landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingComponent } from './components/landing/landing.component';
 
-
+const routes = [
+  {
+    path: '',
+    component: LandingComponent
+  }
+];
 
 @NgModule({
   declarations: [],
